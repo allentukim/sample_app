48b062e415af92807734f8d6072afc6588ca04bd
Allen Kim <allentukim@gmail.com>
2012-03-08 00:25:43 -0800

    Done with sign in

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -35,7 +35,6 @@ describe UsersController do
   end
 
   describe "GET 'new'" do
-
     it "should be successful" do
       get :new
       response.should be_success
@@ -91,6 +90,11 @@ describe UsersController do
       it "should have a welcome message" do
         post :create, :user => @attr
         flash[:success].should =~ /welcome to the sample app/i
+      end
+
+      it "should sign the user in" do
+        post :create, :user => @attr
+        controller.should be_signed_in
       end
     end
   end
